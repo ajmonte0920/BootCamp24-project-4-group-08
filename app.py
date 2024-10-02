8234d9df34f054a48fb31b6d56ad75a30af83ad9
ajmonte0920 <ajmonte0920@gmail.com>
2024-10-01 21:09:30 -0500

    BootCamp24-project-4-group-08

--- app.py
+++ app.py
@@ -1,41 +1,29 @@
 import flask
 from flask import Flask, render_template, redirect, request, jsonify
-import modelHelper
-from modelHelper import ModelHelper
 
 # Create an instance of Flask
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-modelHelper = ModelHelper()
 
 # Route to render index.html template using data from Mongo
+
 @app.route("/")
+def index():
+    return render_template("./templates/index.html")
+
+@app.route("/index")
 def home():
     return render_template("index.html")
 
 @app.route("/about_us")
 def about_us():
-    return render_template("about_us.html")
+    return render_template("templates/about_us.html")
 
 @app.route("/tableau")
 def tableau():
     return render_template("tableau1.html")
 
-@app.route("/recommend", methods=["POST"])
-def recommend():
-    content = request.json["data"]
-    print(content)
-
-    # Extract Title for the recommendation
-    Title = content["Title"]
-
-    recommended_movies = modelHelper.make_recommendation(Title)
-
-    # Convert DataFrame to a JSON-compatible format
-    recommended_movies_json = recommended_movies.to_dict(orient='records')
-
-    return jsonify({"ok": True, "recommendations": recommended_movies_json})
 
 @app.after_request
 def add_header(r):
